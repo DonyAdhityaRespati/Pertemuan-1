ddb59c2cc4a1cfc5809caa4298b2eab52207cf8d
lycensed <55633119+lycensed@users.noreply.github.com>
2019-09-26 16:39:20 +0700

    Add files via upload

---
+++ PBOUL-10117047-Latihan1-BIODATA/src/latihan1/Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author 
+ * NAMA         : Dony Adhitya Respati
+ * KELAS        : PBOULANG
+ * NIM          : 10117047
+ * Deskripsi Program    : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ */
+public class Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------------");
+    System.out.println("NAMA\t\t: Dony Adhitya Respati");
+    System.out.println("ALAMAT\t\t: JL. Cisitu Baru no 9");
+    System.out.println("TEMPAT LAHIR\t: Jambi");
+    System.out.println("TANGGAL LAHIR\t: 5 Juni 1997");
+    System.out.println("-------------------------");
+    System.out.println("HOBI\t\t: Sepeda & Renang");
+    System.out.println("MAKES\t\t: Nasi Goreng");
+    System.out.println("MIKES\t\t: Es Kacang Merah");
+    
+    }
+}
